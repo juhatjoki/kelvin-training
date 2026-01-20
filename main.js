43f4290e670b1b7ba41e85d9b953acd29bd3e79f
Juha Joki <33494330+juhatjoki@users.noreply.github.com>
2026-01-20 23:30:29 +0200

    tidy up the comments

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const kelvin = 300; // set the temperature in kelvins, set it to const so it can't be changed inside the code
-var celsius = kelvin -273; // this line will subtract the correct value from kelvin to get celsius degrees
-var fahrenheit = celsius * (9/5) +32; // this formula gets us the fahrenheit degrees from celsius
-fahrenheit = Math.floor(fahrenheit); // this line will round down the fahrenheit temperature to the nearest whole number
-var newton = celsius * (33/100); // this gives us the temperature in newton scale
-newton = Math.floor(newton); // this line will round down the newton temperature
+// set the temperature in kelvins, set it to const so it can't be changed inside the code
+const kelvin = 300; 
 
-console.log(`The temp in Celsius degrees is ${celsius}. `); //this will print the message and temperature to console
-console.log(`The temp in Fahrenheit degrees is ${fahrenheit}. `); //this will print the message and temperature to console
-console.log(`The temp in Newton degrees is ${newton}. `); //this will print the message and temperature to console
+// this line will subtract the correct value from kelvin to get celsius degrees
+var celsius = kelvin -273; 
+
+// this formula gets us the fahrenheit degrees from celsius, and round it down the fahrenheit temperature to the nearest whole number
+var fahrenheit = celsius * (9/5) +32; 
+fahrenheit = Math.floor(fahrenheit); 
+
+// this gives us the temperature in newton scale and round it down to a whole number
+var newton = celsius * (33/100); 
+newton = Math.floor(newton); 
+
+// print results to console
+console.log(`The temp in Celsius degrees is ${celsius}.`); 
+console.log(`The temp in Fahrenheit degrees is ${fahrenheit}.`); 
+console.log(`The temp in Newton degrees is ${newton}.`); 
